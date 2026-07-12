834ce1e44f6ed2624963ab3041b53aa39f3141c7
Shadow-Solanki <solankitejas152@gmail.com>
2026-07-12 11:33:34 +0530

    add feature:Login

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -9,7 +9,7 @@ from app.database import database
 from app.services.jwt_handler import create_jwt
 from fastapi import Response, status
 
-async def regst(response: Response, data):
+async def auth_regst(response: Response, data):
     db = database()
     con = None
 
@@ -40,6 +40,49 @@ async def regst(response: Response, data):
         return {
             "msg": "Registered Successfully",
             "token": token
+        }
+
+    except Exception as e:
+        print(e)
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"msg": "Internal Server Error"}
+
+    finally:
+        if con:
+            con.close()
+        db.close()
+
+
+async def auth_login(response: Response, data):
+    db = database()
+    con = None
+
+    try:
+        con = db.cursor()
+
+        con.execute(
+            "SELECT email, pass, role FROM user WHERE email = %s",
+            (data.email,)
+        )
+
+        user = con.fetchone()
+
+        if not user:
+            response.status_code = status.HTTP_404_NOT_FOUND
+            return {"msg": "User not found"}
+
+        # user = (email, password, role)
+        if user[1] != data.password:
+            response.status_code = status.HTTP_401_UNAUTHORIZED
+            return {"msg": "Invalid password"}
+
+        token = create_jwt(user[0])
+
+        return {
+            "msg": "Login Successful",
+            "token": token,
+            "email": user[0],
+            "role": user[2]
         }
 
     except Exception as e:
--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from app.schemas.schema import UserData
 from app.api.routes import router as api_router
 from app.core.config import settings
-from app.api.auth import regst
+from app.api.auth import auth_regst,auth_login
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
@@ -22,4 +22,9 @@ app.include_router(api_router)
 
 @app.post("/register")
 async def register(response: Response, data: UserData):
-    return await regst(response, data)
+    return await auth_regst(response, data)
+
+
+@app.post("/login")
+async def login_user(data: UserData, response: Response):
+    return await auth_login(response, data)
